71dd4879bfb6a9297996a363d103c1c3dfe32744
Edu <edu-choba@hotmail.com>
2016-06-12 17:41:23 -0300

    abc blobsBack.h

--- src/blobsBack.h
+++ src/blobsBack.h
@@ -29,19 +29,19 @@ typedef struct {
 } typeBoard;
 
 int canMove(int player, typeBoard *board);
-char* getCommand(typeCommand *command);
-int isInside(int x, int y, int w, int h); //Just for the record
-int validCommand(int player, typeCommand *command, typeBoard *board);
-int move(int player, typeCommand *command, typeBoard *board);
 void conquer(int player, typeCommand *command, typeBoard *board, int blobCount[]);
-void render(typeBoard *board, const int *blobCount,int player);
-int init(typeBoard *board, char *loadedArray);
+int endGame(typeBoard *board, int blobCount[]);
 void fill(typeBoard *board);
 void fillEatAndMove(typeBoard *board);
-void renderMaps(typeBoard *board);
 void getAImove(typeCommand *command, typeBoard *board);
-int endGame(typeBoard *board, int blobCount[]);
-int save(char *filename, int vsAI, int player, int blobCount[], typeBoard *board);
-int load(char *filename, int *vsAI, int *player, int blobCount[], typeBoard *board, char **loadedArray);
+char* getCommand(typeCommand *command);
 char* getFilename();
 int getint(char *message);
+int init(typeBoard *board, char *loadedArray);
+int isInside(int x, int y, int w, int h); //Just for the record
+int load(char *filename, int *vsAI, int *player, int blobCount[], typeBoard *board, char **loadedArray);
+int move(int player, typeCommand *command, typeBoard *board);
+void render(typeBoard *board, const int *blobCount,int player);
+void renderMaps(typeBoard *board);
+int save(char *filename, int vsAI, int player, int blobCount[], typeBoard *board);
+int validCommand(int player, typeCommand *command, typeBoard *board);
